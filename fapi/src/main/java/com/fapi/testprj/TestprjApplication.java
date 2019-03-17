1e162ea3849870bf6a6e039e32e3391ce21389b9
swamnx <swamnx@gmail.com>
2019-03-17 22:43:53 +0300

    Created all modules

---
+++ fapi/src/main/java/com/fapi/testprj/TestprjApplication.java
@@ -0,0 +1,13 @@
+package com.fapi.testprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestprjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestprjApplication.class, args);
+    }
+
+}
---
+++ fapi/src/test/java/com/fapi/testprj/TestprjApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fapi.testprj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestprjApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
